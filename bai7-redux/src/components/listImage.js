64a8e818c68cb161b111072a22fb4febe0e1b773
anh <hotrunganhht1912@gmail.com>
2020-01-08 14:36:00 +0700

    fix  redux

--- bai7-redux/src/components/listImage.js
+++ bai7-redux/src/components/listImage.js
@@ -5,18 +5,23 @@ import { connect } from "react-redux";
 class ListImage extends React.Component {
   render() {
     let data = this.props.dataProduce;
-    return (
-      <div className="row">
-        {data.map((item, index) => (
+    let listDAta;
+    if (data.length > 0) {
+      {
+        listDAta = data.map((item, index) => (
           <ItemImage name={item.name} number={item.number} />
-        ))}
-      </div>
-    );
+        ));
+      }
+    } else {
+      listDAta = <h1>Empty</h1>;
+    }
+    return <div className="row">{listDAta}</div>;
   }
 }
 
 const mapStateToProps = state => {
   let dataProduce = state.productList.filter(item => item.number !== 0);
+  dataProduce.length != 0;
   return {
     dataProduce
   };
